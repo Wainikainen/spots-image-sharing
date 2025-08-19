605f681e499ed243a73b818f86fe879e2fd87e2f
adam wainkainen <awainikainen@gmail.com>
2025-08-19 01:46:21 -0400

    added apis for all the modals and cards

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -4,30 +4,48 @@ class Api {
     this._headers = headers;
   }
 
-  getUserInfo(){
-return fetch (`${this._baseUrl}/users/me`, {
-headers: this._headers,
-})
-.then(this._handleServerResponse)
-.catch(console.error);
-}
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    })
+      .then(this._handleServerResponse)
+      .catch(console.error);
+  }
 
-getInitialCards() {
-  return fetch (`${this._baseUrl}/cards`, {
-  headers:this._headers,
-  })
-  .then(this._handleServerResponse)
-  .catch(console.error);
-}
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    })
+      .then(this._handleServerResponse)
+      .catch(console.error);
+  }
 
+  getSiteInfo() {
+    return Promise.all([this.getInitialCards(), this.getUserInfo()])
+      .then(function ([cards, userInfo]) {
+        return { userInfo, cards };
+      })
+      .catch(console.error);
+  }
 
-getSiteInfo(){
-  return Promise.all([this.getInitialCards(), this.getUserInfo()])
-  .then(function([cards,userInfo]) {
-    return {userInfo, cards};
-  })
-  .catch(console.error);
-}
+  addLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes` ,{
+      method: "PUT",
+      headers: this._headers,
+    })
+    .then(this._handleServerResponse)
+    .catch(console.error);
+  }
+
+  removeLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`,{
+      method: "DELETE",
+      headers: this._headers,
+    })
+    .then(this._handleServerResponse)
+    .catch(console.error);
+  }
+
 
   editUserInfo({ name, about }) {
     return fetch(`${this._baseUrl}/users/me`, {
@@ -39,30 +57,48 @@ getSiteInfo(){
         about,
       }),
     })
-    .then(this._handleServerResponse)
-    .catch(console.error);
-    }
-
-    createCard({name, link}) {
-      return fetch (`${this._baseUrl}/cards`,{
-        method: "POST",
-        headers: this._headers,
-        "Content-Type": "application/json",
-        body: JSON.stringify({
-          name,
-          link,
-        }),
-      })
       .then(this._handleServerResponse)
       .catch(console.error);
-    }
+  }
+
+  editUserAvatar({avatar}) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: this._headers,
+    "Content-Type": "application/json",
+    body: JSON.stringify({
+      avatar,
+    }),
+    })
+    .then(this._handleServerResponse)
+    .catch(console.error);
+  }
 
 
+  createCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      "Content-Type": "application/json",
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    })
+      .then(this._handleServerResponse)
+      .catch(console.error);
+  }
 
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    })
+      .then(this._handleServerResponse)
+      .catch(console.error);
+  }
 
-
-
-   _handleServerResponse(res) {
+  _handleServerResponse(res) {
     if (res.ok) {
       return res.json();
     }
